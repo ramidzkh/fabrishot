c088301256969ed10317d603a8c9e4c352b05c3e
Ramid Khan <ramidzkh@gmail.com>
2021-01-17 21:12:59 +1100

    Update the buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,54 @@
 plugins {
-    id("fabric-loom") version "0.5.26"
+    id("fabric-loom") version "0.6.21"
+    id("org.cadixdev.licenser") version "0.5.0"
 }
 
 group = "me.ramidzkh"
-version = "1.2.2-SNAPSHOT"
+version = "1.2.2"
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    minecraft("net.minecraft", "minecraft", "1.16.5")
+    mappings("net.fabricmc", "yarn", "1.16.5+build.1", classifier = "v2")
+    modImplementation("net.fabricmc", "fabric-loader", "0.11.1")
+
+    modImplementation("net.fabricmc.fabric-api", "fabric-api", "0.29.3+1.16")
+    modImplementation("io.github.prospector", "modmenu", "1.14.13+build.22")
+    modImplementation("me.shedaniel.cloth", "config-2", "4.8.2")
+}
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
-dependencies {
-    minecraft("net.minecraft", "minecraft", "1.16.3")
-    mappings("net.fabricmc", "yarn", "1.16.3+build.40", classifier = "v2")
-    modCompile("net.fabricmc", "fabric-loader", "0.10.1+build.209")
-
-    modImplementation("net.fabricmc.fabric-api", "fabric-api", "0.24.0+build.411-1.16")
-    modImplementation("io.github.prospector:modmenu:1.14.6+build.31")
-    modImplementation("me.shedaniel.cloth:config-2:4.8.2") {
-        exclude(group = "net.fabricmc.fabric-api")
-    }
+license {
+    header = file("LICENSE")
+    include("**/*.java")
 }
 
-tasks.withType<JavaCompile> {
-    options.release.set(8)
+tasks {
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+
+        if (JavaVersion.current().isJava9Compatible) {
+            options.release.set(8)
+        } else {
+            sourceCompatibility = "8"
+            targetCompatibility = "8"
+        }
+    }
+
+    withType<AbstractArchiveTask> {
+        from(rootProject.file("LICENSE"))
+    }
+
+    processResources {
+        filesMatching("fabric.mod.json") {
+            expand("version" to project.version)
+        }
+    }
 }

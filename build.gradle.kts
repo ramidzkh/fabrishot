a8e730bae117805a8bfb1e27b9744d14cdd8e33f
Ramid Khan <ramidzkh@gmail.com>
2021-01-24 09:39:50 +1100

    Update license

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,6 +47,8 @@ tasks {
     }
 
     processResources {
+        inputs.property("version", project.version)
+
         filesMatching("fabric.mod.json") {
             expand("version" to project.version)
         }
--- src/main/java/me/ramidzkh/fabrishot/Fabrishot.java
+++ src/main/java/me/ramidzkh/fabrishot/Fabrishot.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2020 Ramid Khan
+ * Copyright (c) 2021 Ramid Khan
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal

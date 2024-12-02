a02916463a4b8bfe5fda92642043ed0e8043c01c
Ramid Khan <ramidzkh@gmail.com>
2024-12-02 12:46:56 +1100

    Use safer injection point

--- src/main/java/me/ramidzkh/fabrishot/mixins/RenderSystemMixin.java
+++ src/main/java/me/ramidzkh/fabrishot/mixins/RenderSystemMixin.java
@@ -1,12 +1,11 @@
 package me.ramidzkh.fabrishot.mixins;
 
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.mojang.blaze3d.systems.RenderSystem;
 import me.ramidzkh.fabrishot.Fabrishot;
-import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(RenderSystem.class)
 public class RenderSystemMixin {
@@ -14,19 +13,19 @@ public class RenderSystemMixin {
     @Unique
     private static boolean wasLastFrameInCapture;
 
-    @Redirect(method = "flipFrame", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwSwapBuffers(J)V"), remap = false)
-    private static void glfwSwapBuffers(long window) {
+    @WrapWithCondition(method = "flipFrame", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwSwapBuffers(J)V"), remap = false)
+    private static boolean glfwSwapBuffers(long window) {
         if (Fabrishot.isInCapture()) {
             wasLastFrameInCapture = true;
-            return;
+            return false;
         }
 
         // skip a single black frame
         if (wasLastFrameInCapture) {
             wasLastFrameInCapture = false;
-            return;
+            return false;
         }
 
-        GLFW.glfwSwapBuffers(window);
+        return true;
     }
 }
